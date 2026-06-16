28f6a5791acbb1b6d50ee115f9d5647729b1e3e8
derey79 <devyreymond@gmail.com>
2026-06-16 22:08:48 +0700

    add provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,26 +2,21 @@
 
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from '@/context/AuthProvider';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  // Menggunakan useState agar QueryClient tidak terbuat ulang saat re-render di Next.js Client Component
+export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
-            refetchOnWindowFocus: false, // Menghindari fetch ulang otomatis saat pindah tab browser
-            retry: 1, // Mencoba ulang 1 kali jika request gagal
-            staleTime: 5 * 60 * 1000, // Data dianggap segar selama 5 menit
+            staleTime: 60 * 1000, // Data dianggap segar selama 1 menit
+            refetchOnWindowFocus: false, // Jangan fetch ulang otomatis saat pindah tab browser
           },
         },
       })
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>{children}</AuthProvider>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
